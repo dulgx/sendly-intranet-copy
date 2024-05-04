9bbb333104addf39599fe2ad725a5684e3a69ced
Dulguun <dulguun@Dulguuns-MacBook-Air.local>
2024-05-04 11:53:42 +0800

    az

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,10 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  plugins: [
+    nodePolyfills({
+      include: ["path"],
+      exclude: ["http"],
+    }),
+  ],
 });
